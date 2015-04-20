3d274537146998a4a5f365bf1dafc62ff133d46d
hbalazs11 <hbalazs08@gmail.com>
2015-04-20 13:59:49 +0200

    Real-time teszthez beolvasás a billentyűről
    billentyűről az exitProto parancsig való olvasás megvalósítása
    vegrehajt(string) üres, ez csinálja majd a munka nagyrészét

--- src/Main2.java
+++ src/Main2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 
 public class Main2 {
 
@@ -9,13 +11,34 @@ public class Main2 {
 		if(args[0].equals(new String("0"))){
 			System.out.println("Real-time teszt...");
 			
+			
+			Scanner parancsRead = new Scanner(System.in);
+			String parancs;
+			do {
+				System.out.println("Adja meg a parancs kodjat:");
+				if(parancsRead.hasNext())
+		            parancs = parancsRead.next();
+				else { parancs = new String("exitProto");}
+				
+				vegrehajt(parancs);
+				
+				
+			}while (!parancs.equals("exitProto"));
+			
+			
 		}
 		else if(args[0].equals(new String("1"))){
 			System.out.println("File teszt...");
 			
 		}
 		else
-			System.out.println("Hibás argumentumok!");
+			System.out.println("Hibas argumentumok!");
+	}
+
+	private static void vegrehajt(String parancs) {
+		// TODO Auto-generated method stub
+		System.out.println("Parancs jott.");
+		System.out.println(parancs);
 	}
 
 }
