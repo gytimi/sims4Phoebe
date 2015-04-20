282d011dd2cd8bde7f2998581780126afad171bf
Prodinal <rapcsak.prodinal@gmail.com>
2015-04-20 14:34:00 +0200

    System.out.println-ek kikommentelve

--- src/Engine.java
+++ src/Engine.java
@@ -61,7 +61,7 @@ public class Engine {
 	 */
 	
 	public void allPlayersDead(){
-		System.out.println("->[:Engine].allPlayersDead()");
+		//System.out.println("->[:Engine].allPlayersDead()");
 		
 		System.out.println("\nMinenki leesett, nincs gyõztes.\n");
 		}
@@ -74,7 +74,7 @@ public class Engine {
 	 */
 	
 	public void quit(){
-		System.out.println("->[:Engine].quit()");
+		//System.out.println("->[:Engine].quit()");
 		
 		System.exit(0);
 	}
@@ -91,7 +91,7 @@ public class Engine {
 	
 	private void moveRobots(){					//KÉSZ				///VALÓSZÍNŰLEG HIBÁS
 		
-		System.out.println("->[:Engine].moveRobots()");
+		//System.out.println("->[:Engine].moveRobots()");
 		
 		int numberOfRobotsAtStartOfCycle=alivePlayers.size();
 		
@@ -159,7 +159,7 @@ public class Engine {
 	
 	public void roundOver(){					//KÉSZ		//csak a szkeletonhoz public
 		
-		System.out.println("->[:Engine]roundOver()");
+		//System.out.println("->[:Engine]roundOver()");
 		
 		trapRobots();
 		moveRobots();
@@ -169,7 +169,7 @@ public class Engine {
 		
 		round_num--;
 		
-		System.out.println("<-[:Engine].roundOver()");
+		//System.out.println("<-[:Engine].roundOver()");
 	}
 	
 	//PUBLIC, TERVEZETT FÜGGVÉNYEK
@@ -182,7 +182,7 @@ public class Engine {
 	
 	public Engine(){							//KÉSZ
 		
-		System.out.println("->[:Engine]Engine()");
+		//System.out.println("->[:Engine]Engine()");
 		
 		alivePlayers=new ArrayList<Robot>();
 		deadPlayers=new ArrayList<Robot>();
@@ -205,7 +205,7 @@ public class Engine {
 	
 	public void play() {						//PARTIALLY READY
 		
-		System.out.println("->[:Engine].play()");
+		//System.out.println("->[:Engine].play()");
 		
 		while(round_num>0){
 			for(Robot tmp: alivePlayers){
@@ -237,7 +237,7 @@ public class Engine {
 	 */
 	public void init(int numberOfPlayers) {			//KÉSZ
 		
-		System.out.println("->[:Engine].init(numberOfPlayers)");
+		//System.out.println("->[:Engine].init(numberOfPlayers)");
 		
 		map=new Map();
 		map.load("asd");
@@ -265,13 +265,13 @@ public class Engine {
 	 * 
 	 * @param modifier
 	 */
-	public void turnPassed(Coord modifier) {		//TO BE REVIEWED
+	public void turnPassed(Coord modifier) {		//TO BE REVIEWED				//MODIFIED
 		
-		System.out.println("->[:Engine].turnPassed(modifier)");
+		//System.out.println("->[:Engine].turnPassed(modifier)");
 		
 		activePlayer.setModifier(modifier);
 		
-		//notifyAll();			//Szkeletonhoz nem kell
+		notifyAll();			//Szkeletonhoz nem kell
 		
 //		int index=alivePlayers.indexOf(activePlayer);
 //		Robot newActivePlayer=alivePlayers.get((index+1)%alivePlayers.size());		//O.o
@@ -290,7 +290,7 @@ public class Engine {
 	 */
 	public void addTrap(Trap x) {					//KÉSZ
 				
-		System.out.println("->[:Engine].addTrap(x)");
+		//System.out.println("->[:Engine].addTrap(x)");
 		
 		traps.add(x);
 	}
@@ -305,7 +305,7 @@ public class Engine {
 	 */
 	public void dieRobot(Robot r) {					//KÉSZ					//csak a szkeletonban public
 		
-		System.out.println("->[:Engine].dieRobot(r)");
+		//System.out.println("->[:Engine].dieRobot(r)");
 		
 		alivePlayers.remove(r);
 		deadPlayers.add(r);
@@ -322,7 +322,7 @@ public class Engine {
 	
 	public void whoWins() {							//KÉSZ					//csak a szkeletonban public
 		
-		System.out.println("->[:Engine].whoWins()");
+		//System.out.println("->[:Engine].whoWins()");
 		
 		Robot winningPlayer=new Robot(this);
 		for(Robot tmp: alivePlayers){
@@ -333,7 +333,7 @@ public class Engine {
 	}
 
 	public int getPlayer_num() {					//KÉSZ
-		System.out.println("->[:Engine].getPlayer_num()");
+		//System.out.println("->[:Engine].getPlayer_num()");
 		
 		return this.player_num;
 	}
@@ -344,14 +344,14 @@ public class Engine {
 	 */
 	public void setPlayer_num(int player_num) {		//KÉSZ
 		
-		System.out.println("->[:Engine].setPlayer_num(player_num)");
+		//System.out.println("->[:Engine].setPlayer_num(player_num)");
 		
 		this.player_num = player_num;
 	}
 
 	public int getRound_num() {						//KÉSZ
 		
-		System.out.println("->[:Engine].getRound_num()");
+		//System.out.println("->[:Engine].getRound_num()");
 		
 		return this.round_num;
 	}
@@ -362,7 +362,7 @@ public class Engine {
 	 */
 	public void setRound_num(int round_num) {		//KÉSZ
 		
-		System.out.println("->[:Engine].setRound_num(numberOfRounds)");
+		//System.out.println("->[:Engine].setRound_num(numberOfRounds)");
 		
 		this.round_num = round_num;
 	}
@@ -384,7 +384,7 @@ public class Engine {
 
 	public ArrayList<Trap> getTraps() {				//KÉSZ
 		
-		System.out.println("->[:Engine].getTraps()");
+		//System.out.println("->[:Engine].getTraps()");
 		
 		return this.traps;
 	}
@@ -395,7 +395,7 @@ public class Engine {
 	 */
 	public void setTraps(ArrayList<Trap> traps) {	//KÉSZ
 		
-		System.out.println("->[:Engine].setTraps(traps)");
+		//System.out.println("->[:Engine].setTraps(traps)");
 		
 		this.traps = traps;
 	}
