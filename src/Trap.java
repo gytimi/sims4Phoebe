2972bd96f92f939463d9425456427199f694ba54
Aconite94 <hellga94@gmail.com>
2015-04-20 20:30:02 +0200

    Trap

--- src/Trap.java
+++ src/Trap.java
@@ -1,92 +1,67 @@
-public class Trap {
+
+public abstract class Trap {
 
 	private Coord pos;
 	private int r;
 	private Robot owner;
 	private int untilCleaned;
 	private int untilExpiration;
-	private int expired;
+	private Boolean expired;
 
-	/**
-	 * 
-	 * @param r
-	 */
-	public void spring(Robot r) {				//KeSZ
-		System.out.println("->[:Trap].springTrap(r)");
-	}
+	public abstract void spring(Robot r) ;
+	
 
-	/**\brief utkozik-e a robot a csapdaval
-	 * 
-	 * Megnezi, hogy a kapott koordinata
-	 * beleesik-e a csapda koruli r sugaru
-	 * korbe es ennek megfeleloen
-	 * visszater egy bool-lal.
-	 * 
-	 * @param c
-	 */
-	public Boolean collide(Coord c) {			//KeSZ
-		System.out.println("->[:Trap].collide(c)");
-		
-		if((c.getX()-pos.getX())*(c.getX()-pos.getX())+(c.getY()-pos.getY())*(c.getY()-pos.getY())<=r*r){		//(x-x0)^2+(y-y0)^2<=R^2
+	public abstract void timePassed() ;
+	
+
+	public Boolean collide(Coord c) {			
+		if( ( c.getX()-pos.getX()) * (c.getX()-pos.getX() ) +
+				( c.getY()-pos.getY()) * (c.getY()-pos.getY() ) <= r*r ){	//(x-x0)^2+(y-y0)^2<=R^2   
 			return true;
 		}
-		
 		return false;
 	}
 	
-	public void steppedOnByMiniRobot(MiniRobot x){
-		
+
+	public void steppedOnByMiniRobot(MiniRobot x)
+	{
+		x.setOnTrap(true);
+		this.beingCleaned();
 	}
 	
-	public void timePassed(){
-		
+	
+	private void beingCleaned()
+	{
+		if (untilCleaned == 0)
+			this.expired = true;
+		else
+			untilCleaned--;
 	}
 
-	public Coord getPos() {						//KeSZ
-		System.out.println("->[:Trap].getPos()");	
-		
+	
+	public Coord getPos() {								
 		return this.pos;
 	}
 
-	/**
-	 * 
-	 * @param pos
-	 */
-	public void setPos(Coord pos) {				//KeSZ
-		System.out.println("->[:Trap].setPos(pos)");
-		
+
+	public void setPos(Coord pos) {				
 		this.pos = pos;
 	}
 
-	public int getR() {							//KeSZ
-		System.out.println("->[:Trap].getR()");
-		
+	public int getR() {	
 		return this.r;
 	}
 
-	/**
-	 * 
-	 * @param r
-	 */
-	public void setR(int r) {					//KeSZ
-		System.out.println("->[:Trap].setR(r)");
-		
+	public void setR(int r) {
 		this.r = r;
 	}
 
-	public Robot getOwner() {					//KeSZ
-		System.out.println("->[:Trap].getOwner()");
-		
+	public Robot getOwner() {
 		return this.owner;
 	}
 
-	/**
-	 * 
-	 * @param owner
-	 */
-	public void setOwner(Robot owner) {				//KeSZ
-		System.out.println("->[:Trap].setOwner(owner)");
-		
+
+	public void setOwner(Robot owner) {
 		this.owner = owner;
 	}
 
